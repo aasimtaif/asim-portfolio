cde30b75ff1daf55a0f6a5bac2ce1c79ed1b7684
aasimtaif <taifullahansari@gmail.com>
2025-01-12 15:03:19 +0530

    changed resume url

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/16pXaLTEsvSYrmLBda5AxajAbXLoHPXGD/view?usp=sharing"
+              href="https://drive.google.com/file/d/19F9P3i8iYRsunlocubvHLsU5YA0iiNsu/view?usp=sharing"
               target={'_blank'}
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
